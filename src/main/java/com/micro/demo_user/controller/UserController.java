20569fe3ff9a84bfed813e01782aadaa1c800604
alaaalbadry <albadryalaaa@gmail.com>
2025-03-04 14:36:48 +0200

    add new endpoints for different roles for test

--- src/main/java/com/micro/demo_user/controller/UserController.java
+++ src/main/java/com/micro/demo_user/controller/UserController.java
@@ -15,13 +15,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/list")
+    @GetMapping("/public/welcome")
+    public String welcome() {
+        return "Welcome to the public API!";
+    }
+
+    @GetMapping("/user/profile")
+    public String userProfile() {
+        return "This is a user profile. Accessible only by USER role.";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard() {
+        return "Admin dashboard. Accessible only by ADMIN role.";
+    }
+
+    @GetMapping("/admin/list")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    @PostMapping("/create")
+    @PostMapping("/admin/create")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);    }
